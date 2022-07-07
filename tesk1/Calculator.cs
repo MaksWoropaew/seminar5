bd3e13e537c3366ae2b83a59826429bf9354bda4
M.Voropaev <maks.woropaew@mail.ru>
2022-07-07 21:32:41 +0300

    add calculator

---
+++ tesk1/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator
+{
+    public class Cal
+    {
+       public int sum(int x, int y)
+       {
+        int s = x + y;
+        return s;
+       }
+       public int minys(int x, int y)
+       {
+        int m = x - y;
+        return m;
+       }
+    }
+}
+
+
