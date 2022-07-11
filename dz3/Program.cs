2da3e9934bce245bc684d052a2b2a0d00ff595dc
M.Voropaev <maks.woropaew@mail.ru>
2022-07-11 12:03:59 +0300

    add dz3

---
+++ dz3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+namespace Zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Введите число N: ");
+        int numberN = Convert.ToInt32(Console.ReadLine());
+        int sumNumber = SumNumber(numberN);
+        Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+        }
+        static int SumNumber(int numberN)
+        {
+    
+        int count = Convert.ToString(numberN).Length;
+        int advance = 0;
+        int result = 0;
+        for (int i = 0; i < count; i++)
+        {
+            advance = numberN - numberN % 10;
+            result = result + (numberN - advance);
+            numberN = numberN / 10;
+        }
+        return result;
+        }
+    }
+}
