3c85c3a7515519d4974932d5d4f0061cbaa82589
M.Voropaev <maks.woropaew@mail.ru>
2022-07-11 12:02:23 +0300

    add dz2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+namespace Zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.Write("Введите число N: ");
+        int numberN = Convert.ToInt32(Console.ReadLine());
+        int sumNumber = SumNumber(numberN);
+        Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+        }
+        static int SumNumber(int numberN)
+        {
+    
+        int count = Convert.ToString(numberN).Length;
+        int advance = 0;
+        int result = 0;
+        for (int i = 0; i < count; i++)
+        {
+            advance = numberN - numberN % 10;
+            result = result + (numberN - advance);
+            numberN = numberN / 10;
+        }
+        return result;
+        }
+    }
+}
